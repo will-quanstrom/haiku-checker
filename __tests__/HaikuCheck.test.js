bba2b90362dbb29083b46ae35609b2619b46ed40
Student <example@example.com>
2019-11-14 14:04:42 -0800

    program will take in poem, and regradless of spacing between lines, tell how many lines the poem is

--- __tests__/HaikuCheck.test.js
+++ __tests__/HaikuCheck.test.js
@@ -4,7 +4,20 @@ describe('Poem', () => {
 
     test('should correctly indentiy how many lines a poem input has', () => {
         var haiku = new Poem('Ah, tranquility! \n Penetrating the very rock, \n A cicada’s voice.')
-        expect(haiku.checkLines()).toEqual('Your poem has three lines!');
-    })
+        expect(haiku.checkLines()).toEqual('Your poem has 3 lines!');
+    });
+    test('should correctly indentiy how many lines a poem input has', () => {
+        var haiku = new Poem('Ah, tranquility! \n Penetrating the very rock, \n A cicada’s voice \n And heres another line for you.')
+        expect(haiku.checkLines()).toEqual('Your poem does not have 3 lines!');
+    });
+    test('should correctly indentiy how many lines a poem input has', () => {
+        var haiku = new Poem('Ah, tranquility! \n\n Penetrating the very rock, \n\n A cicada’s voice.')
+        expect(haiku.checkLines()).toEqual('Your poem has 3 lines!');
+    });
+    // test('should correctly indentiy how many lines a poem input has', () => {
+    //     var haiku = new Poem('Ah, tranquility! \n\n Penetrating the very rock, \n\n A cicada’s voice.')
+    //     expect(haiku.checkLines()).toEqual('Your poem has 3 lines!');
+    // });
 
-})
+
+});
--- src/haikuCheck.js
+++ src/haikuCheck.js
@@ -3,5 +3,22 @@ export function Poem(text) {
 }
 
 Poem.prototype.checkLines = function () {
+    let writing = this.text
+    writing = writing.split('\n');
+    for(let i = 0; i < writing.length; i++) {
+        if(writing.indexOf("") != -1) {
+            let index = writing.indexOf("");
+            writing.splice(index, 1, );
 
+        } else if(writing.indexOf(" ") != -1) {
+            let index = writing.indexOf(" ");
+            writing.splice(index, 1, );
+        }
+    }
+    if(writing.length == 3){
+    return 'Your poem has 3 lines!';
+    } else {
+        return 'Your poem does not have 3 lines!';
+    }
 }
+
